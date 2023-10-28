56d5bf3d7bf93e429914538d343ce8cb0889b3a9
Marco <phanvutuananh75@gmail.com>
2023-10-29 02:35:21 +0700

    integrate DB data and display successfully

---
+++ pages/components/course-item.php
@@ -0,0 +1,11 @@
+<div class="course">
+    <h2 class="title">This is Courses for register</h2>
+    <?php
+        echo "<p>$courseID</p>";
+        echo "<p>$courseName</p>";
+        echo "<p>$courseType</p>";
+        echo "<p>$facultyID</p>";
+        echo "<p>$lecturerID</p>";
+    ?>
+    
+</div>
