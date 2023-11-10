923162681ad92fbe4b919346c727c1ac16618802
Marco <phanvutuananh75@gmail.com>
2023-11-11 00:27:02 +0700

    finish separate different courseGroup

--- pages/registering-course.php
+++ pages/registering-course.php
@@ -1,8 +1,11 @@
 <div class="displayer container" data-destination="registering-course">
     <h2 class="title">This is Registering-course</h2>
 
+
+
     <?php
         $data_query = "SELECT * FROM course";
+        // $data_query = "SELECT * FROM course WHERE courseID LIKE 'COMP13%'";
         $data_query_result = mysqli_query($connection, $data_query);
 
         $course_list = mysqli_fetch_all($data_query_result, MYSQLI_ASSOC);
@@ -13,7 +16,6 @@
         // echo (gettype($course_list[12]["courseID"]));
 
         foreach ($course_list as $course) {
-            // echo "some shits";
             $courseID = $course["courseID"];
             $courseName = $course["courseName"];
             $courseType = $course["courseType"];
