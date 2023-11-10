e962047946929d60b83fcc04470fc638892b2a9f
Marco <phanvutuananh75@gmail.com>
2023-11-11 00:27:02 +0700

    finish separate different courseGroup

--- pages/components/course-item.php
+++ pages/components/course-item.php
@@ -1,11 +1,20 @@
-<div class="course">
-    <h2 class="title">This is Courses for register</h2>
-    <?php
-        echo "<p>$courseID</p>";
-        echo "<p>$courseName</p>";
-        echo "<p>$courseType</p>";
-        echo "<p>$facultyID</p>";
-        echo "<p>$lecturerID</p>";
-    ?>
+<!-- <div class="course"> -->
+    <?php $courseSingleID = substr($courseID, 0, strlen($courseID) - 2); ?>
+
+    <label class="course-selector flex" for="<?php echo $courseID; ?>">
+        <input type="radio" id="<?php echo $courseID; ?>" name="available-course-for-<?php echo $courseSingleID; ?>">
+        <div class="info-container">
+            <?php
+                echo '<div class="main-info flex flex-row">';
+                    echo "<p>$courseName</p>";
+                    echo "<p>$courseID</p>";
+                echo '</div>';
+                echo "<div>$courseType</div>";
+                // echo "<div>$facultyID</div>";
+                echo "<div>$lecturerID</div>";
+            ?>
+        </div>
+    </label>
+
     
-</div>
+<!-- </div> -->
