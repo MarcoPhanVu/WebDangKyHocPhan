6f2d5891e854412fa6efef536198cd0eebe18089
Marco <phanvutuananh75@gmail.com>
2023-11-16 15:34:38 +0700

    basic styling for courses

--- pages/components/course-item.php
+++ pages/components/course-item.php
@@ -1,12 +1,12 @@
 <!-- <div class="course"> -->
 
-    <label class="course-selector flex" for="<?php echo $courseID; ?>">
+    <label class="course-option flex" for="<?php echo $courseID; ?>" data-group="<?php echo $courseGroup; ?>">
         <input type="radio" id="<?php echo $courseID; ?>" name="available-course-for-<?php echo $courseGroup; ?>">
         <div class="info-container">
             <?php
                 echo '<div class="main-info flex flex-row">';
-                    echo "<p>$courseName</p>";
-                    echo "<p>$courseID</p>";
+                    echo "<div class='mx-4'>$courseName</div>";
+                    echo "<div class='mx-4'>$courseID</div>";
                 echo '</div>';
                 echo "<div>$courseType</div>";
                 // echo "<div>$facultyID</div>";
