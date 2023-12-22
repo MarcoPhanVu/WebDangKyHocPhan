d7c37e31848888095a67dfc69a06aad01e2a8f7e
Marco <phanvutuananh75@gmail.com>
2023-12-22 15:47:36 +0700

    allow requesting for faculties, courses and classes

---
+++ pages/components/class-item.php
@@ -0,0 +1,14 @@
+<button class="class-item" type="submit" name="class-id" value="<?php echo $class_id?>">
+    <?php
+        echo '<div class="main-info flex flex-row">';
+        echo "<p class=''> $course_name";
+        echo "<p class=''> $class_id";
+        echo "<p class=''> $start_session";
+        echo "<p class=''> $end_session";
+        echo "<p class=''> $start_date";
+        echo "<p class=''> $end_date";
+        echo "<p class=''> $lecturer_id";
+        echo "<p class=''> $course_id";
+        echo "<p class=''> $classroom_id";
+    ?>
+</button>
